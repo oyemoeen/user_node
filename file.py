b8dd6ea51fd844153e2fcc56b56f54b88bba04b1
MOEEN ALI <142829791+oyemoeen@users.noreply.github.com>
2025-07-01 16:00:11 +0000

    Update file.py

--- file.py
+++ file.py
@@ -1,3 +1,3 @@
 print("this is the python file")
 print("the 3rd Version of project")
-print("added a feature that will notify the develope after the commit of github"
+print("added a feature that will notify the develope after the commit of github")
